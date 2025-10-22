17e65b8c2519969f0b849f75c239e765041c3c96
MegaLodias <derikaranda@gmail.com>
2025-10-21 22:03:31 -0500

    first commit

---
+++ src/app/auth/losspass/losspass.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-losspass',
+  standalone: true,
+  imports: [],
+  templateUrl: './losspass.component.html',
+  styleUrl: './losspass.component.css'
+})
+export class LosspassComponent {
+
+}
